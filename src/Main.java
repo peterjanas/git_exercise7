4739b39633b4ed354cbe0dc23f2979cff28f3f80
peterjanas <pejanas@gmail.com>
2024-02-01 14:38:24 +0100

    peter push

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,33 @@
 public class Main {
     public static void main(String[] args) {
 
-       printNumbers(1, 10);
+        printNumber(1, 10);
 
     }
 
-    public static void printNumbers(){
-        for(int i = 1; i < 200; i++){
+    public static void printNumbers() {
+        for (int i = 1; i < 200; i++) {
 
 
-            if(i % 3 == 0 && i % 5 == 0){
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("BuzzFizz");
 
-            }
-
-            else if(i % 3 == 0){
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
 
-            }
-
-            else if(i % 5 == 0){
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz!");
 
-            }
-            else{
+            } else {
                 System.out.println(i);
             }
 
         }
     }
 
-    public static void printNumbers(int start, int end){
-       int i = start;
-       while(i <= end){
-           System.out.println(i);
-           i++;
-
-       }
-
-
-       }
+    public static void printNumber(int start, int end) {
+        for (int i = start; i < end+1; i++) {
+            System.out.println(i);
+        }
+    }
 }
