be42bdc0ba623562df69c84a2b350d9ba3556af6
TobiOneCanobi <vlogkongen@gmail.com>
2024-02-01 13:41:08 +0100

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //ALFA OG OMEGA v2.0
+//FIXSHIT
+        printNumbers();
+
+    }
+
+    public static void printNumbers(){
+        for(int i = 1; i < 200; i++){
+
+
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("BuzzFizz");
+
+            }
+
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+
+            }
+
+            else if(i % 5 == 0){
+                System.out.println("Buzz!");
+
+            }
+            else{
+                System.out.println(i);
+            }
+
+        }
     }
 }
